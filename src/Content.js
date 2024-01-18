5aa3c2290f98ba93b41304ba2c60d2acd867d330
akashbalaji369@gmail.com <akashbalaji369@gmail.com>
2024-01-18 09:01:15 +0530

    first commit

---
+++ src/Content.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div>
+        Vanakkam da mapla...
+    </div>
+  )
+}
+
+export default Content
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>___________Tomorrow is what you do today__________</footer>
+  )
+}
+
+export default Footer
